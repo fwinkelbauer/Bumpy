adefb11ccdac957f26685754456a438338e5685b
Florian Winkelbauer <mail@florianwinkelbauer.com>
2017-06-25 13:28:19 +0200

    GH-02 Refactor config initialization

--- Source/Bumpy/Commands.cs
+++ Source/Bumpy/Commands.cs
@@ -10,22 +10,20 @@ namespace Bumpy
 {
     public class Commands
     {
-        private readonly IEnumerable<BumpyConfiguration> _config;
         private readonly IFileUtil _fileUtil;
         private readonly DirectoryInfo _directory;
         private readonly Action<string> _writeLine;
 
-        public Commands(IEnumerable<BumpyConfiguration> config, DirectoryInfo directory, IFileUtil fileUtil, Action<string> writeLine)
+        public Commands(DirectoryInfo directory, IFileUtil fileUtil, Action<string> writeLine)
         {
-            _config = config.ThrowIfNull(nameof(config));
             _directory = directory.ThrowIfNull(nameof(directory));
             _fileUtil = fileUtil.ThrowIfNull(nameof(fileUtil));
             _writeLine = writeLine.ThrowIfNull(nameof(writeLine));
         }
 
-        public void CommandList()
+        public void CommandList(IEnumerable<BumpyConfiguration> config)
         {
-            foreach (var configEntry in _config)
+            foreach (var configEntry in config.ThrowIfNull(nameof(config)))
             {
                 PerformOnContent(configEntry, (content, line, i, readVersion) =>
                 {
@@ -37,21 +35,21 @@ namespace Bumpy
             }
         }
 
-        public void CommandIncrement(int position)
+        public void CommandIncrement(IEnumerable<BumpyConfiguration> config, int position)
         {
-            WriteContent(_config, version => version.Increment(position));
+            WriteContent(config, version => version.Increment(position));
         }
 
-        public void CommandAssign(int position, int number)
+        public void CommandAssign(IEnumerable<BumpyConfiguration> config, int position, int number)
         {
-            WriteContent(_config, version => version.Assign(position, number));
+            WriteContent(config, version => version.Assign(position, number));
         }
 
-        public void CommandWrite(string versionText)
+        public void CommandWrite(IEnumerable<BumpyConfiguration> config, string versionText)
         {
             var newVersion = VersionHelper.ParseVersionFromText(versionText);
 
-            WriteContent(_config, version => newVersion);
+            WriteContent(config, version => newVersion);
         }
 
         public void CommandCreateConfig()
@@ -83,7 +81,7 @@ namespace Bumpy
         {
             List<FileContent> newContent = new List<FileContent>();
 
-            foreach (var configEntry in config)
+            foreach (var configEntry in config.ThrowIfNull(nameof(config)))
             {
                 newContent.AddRange(TransformContent(configEntry, transformFunction));
             }
