cd8c0baebd5d1c263f8fd2c8dd0c44f8a851feca
Florian Winkelbauer <mail@florianwinkelbauer.com>
2017-09-07 17:41:04 +0200

    Fix typo in help command

--- Source/Bumpy/Commands.cs
+++ Source/Bumpy/Commands.cs
@@ -65,7 +65,7 @@ namespace Bumpy
             builder.AppendLine("Usage:");
             builder.AppendLine("  -l");
             builder.AppendLine("    Lists all versions");
-            builder.AppendLine("   -p");
+            builder.AppendLine("  -p");
             builder.AppendLine("    Lists all defined profiles");
             builder.AppendLine("  -c");
             builder.AppendLine("    Creates a .bumpyconfig file if it does not exist");
